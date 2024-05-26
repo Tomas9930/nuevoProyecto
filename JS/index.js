faba900856397b7ddaf7e2ccf73f1c4465aff12b
Tomas9930 <105018764+Tomas9930@users.noreply.github.com>
2024-05-26 19:48:50 -0300

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,48 @@
+
+
+var cad=  `
+
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<div class="container-fluid">
+  <a class="navbar-brand" href="#">Galeria de arte web</a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link" href="../HTML/index.html">Quienes somos?</a>
+        <li class="nav-item">
+            <a class="nav-link" href="../HTML/galeria.html">Galeria</a>
+          </li>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../HTML/aliados.html">Aliados</a>
+        <li class="nav-item">
+            <a class="nav-link" href="../HTML/Sumate.html">Sumate</a>
+      </li>
+     
+      </li>
+    </ul>
+  </div>
+</div>
+</nav>
+
+`
+document.getElementById("idheader").innerHTML=cad;
+
+
+var cad2= `
+<p>
+
+        Nuestras oficinas:Bermúdez 2651, B1417 Cdad. Autónoma de Buenos Aires
+                  
+        Telefono: 011 4566-9569
+
+    </p>
+
+`
+
+document.getElementById("idfooter").innerHTML=cad2;
